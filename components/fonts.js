54bcc839a676ab2884b4f6e4c0d214892d64a5b3
AlexanderChao14 <chaoalexander45@gmail.com>
2023-09-17 05:50:37 -0600

    Started to look for new fonts

--- components/fonts.js
+++ components/fonts.js
@@ -1,6 +1,11 @@
 const Fonts = () => (
     <style jsx global>{`
       @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
+      body{
+        font-family: motiva-sans, sans-serif;
+        font-style: normal;
+        font-weight: 400;
+      }
     `}</style>
   )
 export default Fonts
